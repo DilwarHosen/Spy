94cb3a9df93f2683895f6f4996e6c0f514bd014c
DilwarHosen <105662694+DilwarHosen@users.noreply.github.com>
2025-01-09 13:53:18 +0530

    Update play.py

--- Spy/utils/inline/play.py
+++ Spy/utils/inline/play.py
@@ -33,25 +33,25 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "♡╌╌╌╌╌╌╌╌"
+        bar = "✄·─·─·─·─·─·─·─·─·─"
     elif 10 < umm < 20:
-        bar = "╌♡╌╌╌╌╌╌╌"
+        bar = "-ˋˏ✄·─·─·─·─·─·─·─·─"
     elif 20 <= umm < 30:
-        bar = "╌╌♡╌╌╌╌╌╌"
+        bar = "-ˋˏ-ˋˏ✄·─·─·─·─·─·─·─"
     elif 30 <= umm < 40:
-        bar = "╌╌╌♡╌╌╌╌╌"
+        bar = "-ˋˏ-ˋˏ-ˋˏ✄·─·─·─·─·─·─"
     elif 40 <= umm < 50:
-        bar = "╌╌╌╌♡╌╌╌╌"
+        bar = "-ˋˏ-ˋˏ-ˋˏ-ˋˏ✄·─·─·─·─·─"
     elif 50 <= umm < 60:
-        bar = "╌╌╌╌╌♡╌╌╌"
+        bar = "-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ✄·─·─·─·─"
     elif 60 <= umm < 70:
-        bar = "╌╌╌╌╌╌♡╌╌"
+        bar = "-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ✄·─·─·─"
     elif 70 <= umm < 80:
-        bar = "╌╌╌╌╌╌╌♡╌"
+        bar = "-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ✄·─·─"
     elif 80 <= umm < 95:
-        bar = "╌╌╌╌╌╌╌╌♡╌"
+        bar = "-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ✄·─"
     else:
-        bar = "╌╌╌╌╌╌╌╌╌♡"
+        bar = "-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ-ˋˏ✄·"
     buttons = [
         [
             InlineKeyboardButton(
@@ -60,16 +60,21 @@ def stream_markup_timer(_, chat_id, played, dur):
             )
         ],
         [
+            InlineKeyboardButton(text="❚❚", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
         ],
         [
-            InlineKeyboardButton(text="ʀᴇᴘʟᴀʏ[↻]", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="ᴄʟᴏsᴇ[✗]", callback_data="close"
-            )
+            InlineKeyboardButton(
+                text="[🇮🇳] Aʟᴏɴᴇ", url=f"https://t.me/AloneHuVai"
+            ),
+            InlineKeyboardButton(
+                text="Uᴘᴅᴀᴛᴇs", url=f"https://t.me/AloneXBots"
+            ),
         ],
+        [InlineKeyboardButton(text="ᴄʟᴏsᴇ[✗]", callback_data="close")],
     ]
     return buttons
 
@@ -77,13 +82,21 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
+            InlineKeyboardButton(text="❚❚", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
         ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+        [
+            InlineKeyboardButton(
+                text="[🇮🇳] Aʟᴏɴᴇ", url=f"https://t.me/AloneHuVai"
+            ),
+            InlineKeyboardButton(
+                text="Uᴘᴅᴀᴛᴇs", url=f"https://t.me/AloneXBots"
+            ),
+        ],
+        [InlineKeyboardButton(text="ᴄʟᴏsᴇ[✗]", callback_data="close")],
     ]
     return buttons
 

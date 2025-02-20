6b64bb685a11c897c12657c4c02f3f0a7d50f5ba
DilwarHosen <105662694+DilwarHosen@users.noreply.github.com>
2025-02-20 14:46:44 +0530

    Update call.py

--- Spy/core/call.py
+++ Spy/core/call.py
@@ -3,13 +3,14 @@ import os
 from datetime import datetime, timedelta
 from typing import Union
 
+from ntgcalls import TelegramServerError
 from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup
 from pytgcalls import PyTgCalls, StreamType
 from pytgcalls.exceptions import (
     AlreadyJoinedError,
     NoActiveGroupCall,
-    TelegramServerError,
+    #TelegramServerError,
 )
 from pytgcalls.types import Update
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
